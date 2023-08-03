7d4857ba8345f74ddd3806b37a26be2f9c8e1148
jjesusbautista <jesus.bautista@pirelli.com>
2023-08-02 23:01:44 -0600

    info blueprint flask-asset

--- app_practice/assets.py
+++ app_practice/assets.py
@@ -5,12 +5,13 @@ from flask_assets import Bundle
 def compile_static_assets(assets):
     assets.auto_build = True
     assets.debug = False
-    # common_style_bundle = Bundle(
-    #     "src/less/*.less",
-    #     filters="less,cssmin",
-    #     output="dist/css/style.css",
-    #     extra={"rel": "stylesheet/less"},
-    # )
+
+    info_style_bundle = Bundle(
+        "info_bp/css/info.css",
+        filters="cssmin",
+        output="dist/css/info.css",
+        extra={"rel": "stylesheet/less"},
+    )
     home_style_bundle = Bundle(
         "home_bp/css/home.css",
         filters="cssmin",
@@ -19,7 +20,9 @@ def compile_static_assets(assets):
     )
 
     assets.register("home_style_bundle", home_style_bundle)
+    assets.register("info_style_bundle", info_style_bundle)
 
     if app.config["FLASK_ENV"] == "development":
         home_style_bundle.build()
+        info_style_bundle.build()
     return assets
